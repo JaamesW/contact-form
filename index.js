d59942b458107a81f0cba4da3c00ac0c1dbd8cb4
James <james.williams@zywave.com>
2017-09-28 07:47:21 -0500

    update

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ $("#imgInp").change(function(){
 $(document).ready(function() {
     var wrapper         = $(".phone"); 
     var add_button      = $(".add-phone"); 
-	var newInput = ('<div class="button-action-wrap-added"><select class="f-select"><option value="work">Work</option> <option value="mobile">Mobile</option> <option value="home">Home</option></select><input type="text" class="f-input large" name="mytext[]"/><div id="primary" class="primary-phone">primary</div><button href="#" class="remove_field"><svg fill="#F96060" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"> <path d="M0 0h24v24H0z" fill="none"/> <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11H7v-2h10v2z"/></svg></button></div>')
+	var newInput = ('<div class="button-action-wrap-added"><select class="f-select"><option value="work">Work</option> <option value="mobile">Mobile</option> <option value="home">Home</option></select><input type="text" class="f-input small" name="mytext[]"/><input class="f-input xsmall" type="tel" placeholder="Ext" name="mytext[]"><div id="primary" class="primary-phone">primary</div><button href="#" class="btn--remove"><svg fill="#F96060" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"> <path d="M0 0h24v24H0z" fill="none"/> <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11H7v-2h10v2z"/></svg></button></div>')
 	
     $(add_button).click(function(e){
         e.preventDefault();
 		$(this).parent().parent(wrapper).append(newInput); 
     });
     
-    $(wrapper).on("click",".remove_field", function(e){ 
+    $(wrapper).on("click",".btn--remove", function(e){ 
         e.preventDefault(); $(this).parent('div').remove();
     })
 });
@@ -33,10 +33,10 @@ $(document).ready(function() {
 	
     $(add_button).click(function(e){
         e.preventDefault();
-		$(this).parent().parent(wrapper).append('<div class="button-action-wrap-added"><input type="text" class="f-input large" name="mytext[]"/><div id="primary" class="primary-email">primary</div><button href="#" class="remove_field"><svg fill="#F96060" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"> <path d="M0 0h24v24H0z" fill="none"/> <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11H7v-2h10v2z"/></svg></button></div>'); 
+		$(this).parent().parent(wrapper).append('<div class="button-action-wrap-added"><input type="text" class="f-input large" name="mytext[]"/><div id="primary" class="primary-email">primary</div><button href="#" class="btn--remove"><svg fill="#F96060" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"> <path d="M0 0h24v24H0z" fill="none"/> <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11H7v-2h10v2z"/></svg></button></div>'); 
     });
     
-    $(wrapper).on("click",".remove_field", function(e){ 
+    $(wrapper).on("click",".btn--remove", function(e){ 
         e.preventDefault(); $(this).parent('div').remove();
     })
 });
